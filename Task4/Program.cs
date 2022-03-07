644517a9d2a138adae1aaafaa679747dfd75874b
Stitch710 <dima_pichugin_2016@mail.ru>
2022-03-07 13:10:24 +0300

    Добавьте файлы проекта.

---
+++ Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите кол-во треугольников");
+            if (int.TryParse(Console.ReadLine(), out int n) && n > 0)
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    for (int j = 0; j < i; j++)
+                    {
+                        string b = new String('*', j);
+                        Console.WriteLine(b.PadLeft(n) + "*" + b);
+                    }
+                }
+            }
+            else
+                Console.WriteLine("введите коректное число");
+        }
+    }
+}
